3a66a81ca13c1bd09d328cf3030e93ab07650c71
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-11-25 22:13:13 +0200

    Update navigateToPage.js

--- script/navigateToPage.js
+++ script/navigateToPage.js
@@ -1,18 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("FormLogin").addEventListener("submit", function (event) {
         event.preventDefault();
-        window.location.href = "examPage.html";
+        window.location.replace("examPage.html");
+
 
     });
-
-    window.history.pushState(null, null, window.location.href);
-
-    window.addEventListener("popstate", function () {
-        // Redirect or keep user on the current page
-        window.history.pushState(null, null, window.location.href);
+    document.getElementById("FormSignup").addEventListener("submit", function (event) {
+        event.preventDefault();
+        window.location.replace("examPage.html");
     });
-
-
-
-})
-
+}
+);
