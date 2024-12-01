228ad5796bf4822e590f89a3e57ca084c552887a
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-12-01 21:21:23 +0200

    Update navigateToPage.js

--- script/navigateToPage.js
+++ script/navigateToPage.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("FormLogin").addEventListener("submit", function (event) {
         event.preventDefault();
-        window.location.replace("examPage.html");
+        window.location.replace("welcomePage.html");
 
 
     });
     document.getElementById("FormSignup").addEventListener("submit", function (event) {
         event.preventDefault();
-        window.location.replace("examPage.html");
+        window.location.replace("welcomePage.html");
     });
+
 }
 );
+document.getElementById("welcome").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.replace("examPage.html");
+});
 
