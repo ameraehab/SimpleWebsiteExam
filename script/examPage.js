ce34d0bea5a64a7dfb9f74ce9c763d1217d92622
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-12-03 12:44:20 +0200

    Update examPage.js

--- script/examPage.js
+++ script/examPage.js
@@ -4,17 +4,11 @@ class exam {
         this.examContant = document.querySelector(".exam-contant");
         this.nextBtn = document.querySelector(".next-btn");
         this.prevBtn = document.querySelector(".previous-btn");
-        this.answerBtn = document.querySelector(".answerBtn");
         this.index = 0;
-        this.totalAnswers = 0;
         this.loading = 0;
-        this.load = document.querySelector(".loading");
-        this.p = document.createElement("p");
-        this.loadDiv = document.createElement("div");
-        this.load.appendChild(this.loadDiv);
         this.fetchData();
-        this.userSide = document.querySelector(".user");
-        this.flagBtn = document.querySelector(".flag");
+
+
 
     }
     async fetchData() {
@@ -55,10 +49,16 @@ class exam {
                     <div class="answer-D"> <button id="btn-ans-4" class="answerBtn">${question.options.D}</button>    </div>
                 </div> 
                 `;
-
-
-
         this.examContant.appendChild(div);
+        const selectAnswerBnt = div.querySelectorAll(".answerBtn");
+
+        selectAnswerBnt.forEach((button) => {
+            button.addEventListener("click", (event) => {
+                selectAnswerBnt.forEach((btn) => btn.classList.remove('active'));
+                event.target.classList.add("active");
+
+            });
+        });
         if (this.index == 0) {
             this.prevBtn.style.display = "none";
 
@@ -66,138 +66,11 @@ class exam {
             this.nextBtn.style.display = "none";
 
         }
-        this.checkCorrectAnswer(question);
-        this.p.textContent = `${this.loading} %`;
-        this.load.appendChild(this.p);
-        this.p.style.cssText = "font-Weight:50px;position: fixed;font-Size:small; padding:3px  ";
-
-    }
 
 
-
-    checkCorrectAnswer(question) {
-        const btnA = document.getElementById("btn-ans-1");
-        const btnB = document.getElementById("btn-ans-2");
-        const btnC = document.getElementById("btn-ans-3");
-        const btnD = document.getElementById("btn-ans-4");
-        const a = Object.keys(question.options);
-
-        btnA.addEventListener("click", () => {
-            this.updateButtonStyles(btnA, [btnB, btnC, btnD]);
-            this.handleAnswer(a[0], question.correctAnswer);
-            this.updateLoadingStyles();
-        });
-        btnB.addEventListener("click", () => {
-            this.updateButtonStyles(btnB, [btnA, btnC, btnD]);
-            this.handleAnswer(a[1], question.correctAnswer);
-            this.updateLoadingStyles();
-        });
-        btnC.addEventListener("click", () => {
-            this.updateButtonStyles(btnC, [btnA, btnB, btnD]);
-            this.handleAnswer(a[2], question.correctAnswer);
-            this.updateLoadingStyles();
-
-        });
-        btnD.addEventListener("click", () => {
-            this.updateButtonStyles(btnD, [btnA, btnB, btnC]);
-            this.handleAnswer(a[3], question.correctAnswer);
-            this.updateLoadingStyles();
-
-        });
 
 
     }
-    updateButtonStyles(selectedBtn, otherBtns) {
-        selectedBtn.style.backgroundColor = "rgb(212, 212, 212)";
-
-        otherBtns.forEach(btn => btn.style.backgroundColor = "aliceblue");
-
-        sessionStorage.setItem(`selectedBtn_${this.index}`, selectedBtn.id);
-        sessionStorage.setItem(`selectedBtnStyle_${this.index}`, "rgb(212, 212, 212)");
-
-        otherBtns.forEach(btn => {
-            sessionStorage.setItem(`btn_${btn.id}_style_${this.index}`, "aliceblue");
-        });
-    }
-
-    restoreButtonStyles() {
-        const selectedBtnId = sessionStorage.getItem(`selectedBtn_${this.index}`);
-        const selectedBtnStyle = sessionStorage.getItem(`selectedBtnStyle_${this.index}`);
-
-        if (selectedBtnId && selectedBtnStyle) {
-            const selectedBtn = document.getElementById(selectedBtnId);
-            if (selectedBtn) {
-                selectedBtn.style.backgroundColor = selectedBtnStyle;
-            }
-        }
-
-        window.querySelectorAll('.answerBtn').forEach(btn => {
-            const btnStyle = sessionStorage.getItem(`btn_${btn.id}_style_${this.index}`);
-            if (btnStyle) {
-                btn.style.backgroundColor = btnStyle;
-            }
-        });
-    }
-    updateLoadingStyles() {
-        if (this.loading == 10) {
-            this.loadDiv.style.cssText = " float: left;width: 10%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-        } else if (this.loading == 20) {
-            this.loadDiv.style.cssText = " float: left;width: 20%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-        } else if (this.loading == 30) {
-            this.loadDiv.style.cssText = " float: left;width: 30%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 40) {
-            this.loadDiv.style.cssText = " float: left;width: 40%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 50) {
-            this.loadDiv.style.cssText = " float: left;width: 50%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 60) {
-            this.loadDiv.style.cssText = " float: left;width: 60%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 70) {
-            this.loadDiv.style.cssText = " float: left;width: 70%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 80) {
-            this.loadDiv.style.cssText = " float: left;width: 80%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 90) {
-            this.loadDiv.style.cssText = " float: left;width: 90%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
-
-
-        } else if (this.loading == 100) {
-            this.loadDiv.style.cssText = " float: left;width: 100%;height: 18px;background-color: rgb(222, 222, 222);border-radius: 25px;position: relative;";
-            this.loading = 0;
-
-
-        }
-
-    }
-
-    handleAnswer(selectedOption, correctAnswer) {
-        this.loading += 10;
-
-        this.p.textContent = `${this.loading} %`;
-        this.load.appendChild(this.p);
-        this.p.style.cssText = "font-Weight:50px;position: fixed;font-Size:small; padding:3px  ";
-
-        if (selectedOption === correctAnswer) {
-            console.log(`Correct answer: ${correctAnswer}`);
-            this.totalAnswers++;
-        } else {
-            console.log(`Incorrect answer.`);
-        }
-
-    }
-
 
     nextButton() {
 
@@ -256,3 +129,116 @@ let quizApp = new exam();
 
 
 
+
+
+
+
+// checkCorrectAnswer(question) {
+//     const btnA = document.getElementById("btn-ans-1");
+//     const btnB = document.getElementById("btn-ans-2");
+//     const btnC = document.getElementById("btn-ans-3");
+//     const btnD = document.getElementById("btn-ans-4");
+//     const a = Object.keys(question.options);
+
+//     btnA.addEventListener("click", () => {
+//         this.updateButtonStyles(btnA, [btnB, btnC, btnD]);
+//         this.handleAnswer(a[0], question.correctAnswer);
+
+//     });
+//     btnB.addEventListener("click", () => {
+//         this.updateButtonStyles(btnB, [btnA, btnC, btnD]);
+//         this.handleAnswer(a[1], question.correctAnswer);
+//         this.updateLoadingStyles();
+
+//     });
+//     btnC.addEventListener("click", () => {
+//         this.updateButtonStyles(btnC, [btnA, btnB, btnD]);
+//         this.handleAnswer(a[2], question.correctAnswer);
+
+//     });
+//     btnD.addEventListener("click", () => {
+//         this.updateButtonStyles(btnD, [btnA, btnB, btnC]);
+//         this.handleAnswer(a[3], question.correctAnswer);
+//     });
+//     this.updateLoadingStyles(this.index);
+
+
+
+
+
+
+// }
+
+
+
+// updateLoadingStyles() {
+//     this.loading = this.loading + 10;
+//     this.loadDiv.style.cssText = ` float: left;width:${this.loading};height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;`;
+//     this.loadDiv.innerHTML = `${this.loading} %`
+
+//     this.load.appendChild(this.loadDiv);
+
+//     if (this.loading >= 100) {
+//         this.loading = 0;
+//     }
+//     // if (this.loading == 10) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 10%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+//     // } else if (this.loading == 20) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 20%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+//     // } else if (this.loading == 30) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 30%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 40) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 40%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 50) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 50%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 60) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 60%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 70) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 70%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 80) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 80%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 90) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 90%;height: 18px;background-color:  rgb(222, 222, 222);border-radius: 25px;position: relative;";
+
+
+//     // } else if (this.loading == 100) {
+//     //     this.loadDiv.style.cssText = " float: left;width: 100%;height: 18px;background-color: rgb(222, 222, 222);border-radius: 25px;position: relative;";
+//     //     this.loading = 0;
+
+
+//     // }
+
+
+// }
+
+
+
+
+
+
+
+// handleAnswer(selectedOption, correctAnswer) {
+
+
+//     if (selectedOption === correctAnswer) {
+//         console.log(`Correct answer: ${correctAnswer}`);
+//         this.totalAnswers++;
+//     } else {
+//         console.log(`Incorrect answer.`);
+//     }
+
+// }
